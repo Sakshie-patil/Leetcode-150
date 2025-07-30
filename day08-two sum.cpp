aa729bccf4a0d09eefb5fcd79736cbe6ae53a10d
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-07-30 15:26:01 +0530

    Update day08-two sum.cpp

--- day08-two sum.cpp
+++ day08-two sum.cpp
@@ -7,7 +7,7 @@ vector<int> twoSum(vector<int>& arr, int target) {
             if(arr[left] + arr[right] == target)  {
                 ans.push_back(left+1);
                 ans.push_back(right+1);
-                break;
+                break;        // if we don't add this break here then will get TLE because loop will continue even after getting the answer
             }
             else if(arr[left] + arr[right] > target) {
                 right--;
