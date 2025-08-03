9d0c7d98dace4d6309e5c408a78244317b0858f3
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-08-03 10:33:16 +0530

    Create day10-maxximum subarray sum.cpp

---
+++ day10-maxximum subarray sum.cpp
@@ -0,0 +1,15 @@
+int maxSubArray(vector<int>& arr) {
+        int n = arr.size();
+        int sum=0;
+        int maxSum=INT_MIN;
+
+        for(int i=0;i<n;i++) {
+            sum = sum + arr[i];
+            maxSum = max(maxSum, sum);
+        
+        if(sum < 0) {
+            sum =0;
+        }
+    }
+        return maxSum;
+    }
