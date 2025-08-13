d0ca8058399131afce6e88b3a5ce7b99d451f74a
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-08-13 18:44:38 +0530

    Create dat14-peak element.cpp

---
+++ dat14-peak element.cpp
@@ -0,0 +1,23 @@
+int findPeakElement(vector<int>& arr) {
+        int n = arr.size();
+        int low = 1;
+        int high = n-2;
+        if(n==1) return 0;
+        // if peak is present at first or last index
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        
+        while(high >= low) {
+            int mid = low + (high-low)/2;
+            if(arr[mid-1] <= arr[mid] && arr[mid] >= arr[mid+1]) {
+                return mid;
+            }
+            else if(arr[mid-1] <= arr[mid] && arr[mid] <= arr[mid+1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
