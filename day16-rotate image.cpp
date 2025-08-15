94065200d18f17e56988a37b344b2bc66125f8df
Sakshie-patil <141345951+Sakshie-patil@users.noreply.github.com>
2025-08-15 11:16:35 +0530

    Create day16-rotate image.cpp

---
+++ day16-rotate image.cpp
@@ -0,0 +1,14 @@
+void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        //transpose
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+
+        // swap
+        for(int i=0; i<n; i++) {
+            reverse(mat[i].begin(), mat[i].end());
+        }
+    }
